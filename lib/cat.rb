0cc25efdcbe8af9024063a3b2613c73832ee1532
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-10 22:47:04 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name,owner)
+    @species = "cat"
+    @name = name
+    @owner = owner
+    owner.cats << self
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name,owner)
+    @species = "dog"
+    @name = name
+    @owner = owner
+    owner.dogs << self
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
