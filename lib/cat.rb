c6884a91bf594501f0aa90ec4bac63854cf6458f
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-20 14:36:09 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,30 @@
 class Cat
+  
+  
+  attr_accessor :owner, :mood
+  
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  #def self.cats
+  #  Cat.all.count
+  #end
+  
+  #def self.count
+  #  @@all << self
+  #end
+    
   # code goes here
 end
